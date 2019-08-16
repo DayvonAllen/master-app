fab076169cff2ba4658b7c192fb84e600cc495f2
DayvonAllen <39011012+DayvonAllen@users.noreply.github.com>
2019-08-16 10:01:57 -0700

    Update sha-advanced.page.ts

--- src/app/sha-advanced/sha-advanced.page.ts
+++ src/app/sha-advanced/sha-advanced.page.ts
@@ -31,7 +31,7 @@ export class ShaAdvancedPage implements OnInit {
   }
   playSound(){
     this.front = null;
-    this.snd = new Audio(`https://hiragana-api.herokuapp.com/sounds/${this.list[0]['romanji']}`)
+    this.snd = new Audio(`localhost${this.list[0]['romanji']}`)
     this.snd.play()
   }
 
